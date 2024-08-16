95d432d8b5f5a749211fc89ecb7ca4970f040faf
Peter J. Hart <peter@peterjhart.com>
2024-08-15 17:46:10 -0700

    Code cleanup.

--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"regexp"
 )
 
 func redirectHandler(writer http.ResponseWriter, request *http.Request) {
-	log.Printf("Handling route %s", request.URL.Path)
+	log.Println("Handling route %s", request.URL.Path)
 
 	if request.URL.Path == "/" {
 		http.ServeFile(writer, request, "./dist/index.html")
@@ -24,18 +23,21 @@ func redirectHandler(writer http.ResponseWriter, request *http.Request) {
 
 	writer.WriteHeader(http.StatusNotFound)
 	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-	json.NewEncoder(writer).Encode(map[string]string{"error": "Coming Soon"})
+	err := json.NewEncoder(writer).Encode(map[string]string{"error": "Coming Soon"})
+	if err != nil {
+		log.Println("Error encoding JSON:", err)
+		return
+	}
 	return
 }
 
 func main() {
-	//http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./dist"))))
 	http.Handle("/admin/", http.StripPrefix("/admin/", http.FileServer(http.Dir("./dist/admin"))))
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./dist/assets"))))
 
 	http.HandleFunc("/", redirectHandler)
 
-	fmt.Println("Listening on port 8080")
+	log.Println("Listening on port 8080")
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("There was a problem starting the server:", err)
