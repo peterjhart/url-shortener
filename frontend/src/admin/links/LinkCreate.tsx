868b2428b44910d30b41698659055a88ff2ebe7e
Peter J. Hart <peter@peterjhart.com>
2024-08-23 17:48:59 -0700

    Stub out form.

--- frontend/src/admin/links/LinkCreate.tsx
+++ frontend/src/admin/links/LinkCreate.tsx
@@ -12,6 +12,33 @@ export default function LinkCreate(): ReactElement {
       <div>
         <Link to="/links">Back to Links</Link>
       </div>
+      <form>
+        <div className="mb-4">
+          <label htmlFor="input_alias" className="block">
+            Alias
+          </label>
+          <input
+            type="text"
+            id="input_alias"
+            className="border border-gray-400 rounded p-1"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="input_link" className="block">
+            Link
+          </label>
+          <input
+            type="text"
+            id="input_link"
+            className="w-1/2 border border-gray-400 rounded p-1"
+          />
+        </div>
+        <div className="mb-4">
+          <button className="text-white bg-fuchsia-800 hover:bg-fuchsia-600 p-3 border-0 rounded-md">
+            Create
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
